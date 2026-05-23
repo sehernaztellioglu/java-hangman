5600818e0bd6964c7fde10f65cbafd680202fdf9
sehernaztellioglu <sehernaztellioglu@gmail.com>
2026-05-23 22:43:12 +0300

    asdasddasd

--- NewProject1/untitled/src/Main.java
+++ NewProject1/untitled/src/Main.java
@@ -2,64 +2,65 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 import java.util.Scanner;
 
-class Game{
+class Game {
     String word = "coconut";
     int length = word.length();
-    int x;
+    int x = 0;
 
-        public void myWelcome() {
-            System.out.println("||WELCOME TO THE HANGMAN GAME||");
-            System.out.println("\nYOUR WORD IS: ");
+    public void myWelcome() {
+        System.out.println("||WELCOME TO THE HANGMAN GAME||");
+        System.out.println("\nYOUR WORD IS: ");
+    }
+
+    public void myHangman() {
+
+        if (x == 0) {
+            System.out.printf("%5s", "O\n");
+        } else if (x == 1) {
+            System.out.printf("%5s%3s", "O\n","/\n");
+        } else if (x == 2) {
+            System.out.printf("%5s%3s%s", "O\n","/","|\n");
+        } else if (x == 3) {
+            System.out.printf("%5s%3s%s%s", "O\n","/","|","\\\n");
+        } else if (x == 4) {
+            System.out.printf("%5s%3s%s%s%4s", "O\n","/","|","\\\n","/\n");
+        } else if (x == 5) {
+            System.out.printf("%5s%3s%s%s%3s %s", "O\n","/","|","\\\n","/","\\\n");
         }
-
-        public void myHangman() {
-
-            if (x == 0) {
-                System.out.printf("%5s", "O");
-            } else if (x == 1) {
-                System.out.printf("3%s", "/");
-            } else if (x == 2) {
-                System.out.printf("%5s", "|");
-            } else if (x == 3) {
-                System.out.printf("%7s", "\\");
-            } else if (x == 4) {
-                System.out.printf("3%s", "/");
-            } else if (x == 5) {
-                System.out.printf("%7s", "\\");
-            }
-        }
-
-        public void myWord() {
-
-            for (int i = 0; i < length; i++) {
-                System.out.print("_ ");
-            }
-        }
+    }
 
 
-    public void main(String[] args){
+    public void main(String[] args) {
         myWelcome();
-        myWord();
-
         Scanner myObj = new Scanner(System.in);
-        System.out.println("\n\nENTER A LETTER: ");
 
-        String userLetter = myObj.nextLine();
-        String upperCase = userLetter.toUpperCase();
+        int i = 0;
 
-            for(int i = 0; i < length; i++){
+        while (i < length) {
+            String text = "_ ".repeat(length);
+            System.out.print(text);
+
+            System.out.println("\n\nENTER A LETTER: ");
+
+            String userLetter = myObj.nextLine();
+            String lowerCase = userLetter.toLowerCase();
+
             String c = String.valueOf(word.charAt(i));
 
-            if(c == upperCase){
-                myWord();
 
-                String
+            if (c.equals(lowerCase)) {
+                text = text.replace("c","upperCase");
 
 
+            } else {
+                myHangman();
+                x++;
+                if (x == 6){
+                    System.out.println("YOU LOST");
+                    break;
+                }
             }
+            i++;
         }
-
-
-
     }
-  }
+}
