beae969bda6dc4ff882212d82e9cb2de7e648d83
sehernaztellioglu <sehernaztellioglu@gmail.com>
2026-05-30 01:47:47 +0300

    Hangman Game Project

--- NewProject1/untitled/src/Main.java
+++ NewProject1/untitled/src/Main.java
@@ -1,5 +1,6 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 import java.util.Scanner;
 
 class Game {
@@ -17,20 +18,20 @@ class Game {
         if (x == 0) {
             System.out.printf("%5s", "O\n");
         } else if (x == 1) {
-            System.out.printf("%5s%3s", "O\n","/\n");
+            System.out.printf("%5s%3s", "O\n", "/\n");
         } else if (x == 2) {
-            System.out.printf("%5s%3s%s", "O\n","/","|\n");
+            System.out.printf("%5s%3s%s", "O\n", "/", "|\n");
         } else if (x == 3) {
-            System.out.printf("%5s%3s%s%s", "O\n","/","|","\\\n");
+            System.out.printf("%5s%3s%s%s", "O\n", "/", "|", "\\\n");
         } else if (x == 4) {
-            System.out.printf("%5s%3s%s%s%4s", "O\n","/","|","\\\n","/\n");
+            System.out.printf("%5s%3s%s%s%4s", "O\n", "/", "|", "\\\n", "/\n");
         } else if (x == 5) {
-            System.out.printf("%5s%3s%s%s%3s %s", "O\n","/","|","\\\n","/","\\\n");
+            System.out.printf("%5s%3s%s%s%3s %s", "O\n", "/", "|", "\\\n", "/", "\\\n");
         }
     }
 
 
-    public static String changingLetter(String text, int index, String newChar){
+    public static String changingLetter(String text, int index, String newChar) {
 
         return text.substring(0, index)
                 + newChar
@@ -46,9 +47,7 @@ class Game {
         int i = 0;
         String text = "_".repeat(length);
 
-        int totalPossibility = word.length() + 6;
-
-        while (i <= totalPossibility) {
+        while (true) {
 
             System.out.print(text);
 
@@ -59,9 +58,8 @@ class Game {
 
             boolean changed = false;
 
-            for(int j = 0; j < length; j++) {
+            for (int j = 0; j < length; j++) {
 
-                String checkText = text;
                 String Letters = String.valueOf(word.charAt(j));
 
                 if (Letters.equals(lowerCase)) {
@@ -70,21 +68,19 @@ class Game {
 
                 }
             }
-            if(!changed){
+            if (!changed) {
                 myHangman();
                 x++;
             }
-            if(x == 6){
+            if (x == 6) {
                 System.out.println("YOU LOST");
                 break;
             }
-            i++;
+            if (text.equals(word)) {
 
-                 if (text.equals(word)) {
-
-                     System.out.println("YOU WON");
-                     break;
-                 }
+                System.out.println("YOU WON");
+                break;
             }
         }
     }
+}
